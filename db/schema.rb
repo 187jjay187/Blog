e769af3cc13ff556b797bbfee56a4f6e829a4253
187jjay187 <187jjay187@gmail.com>
2023-03-03 22:20:36 +0200

    Add user ref to comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 202303031184605) do
+ActiveRecord::Schema[7.0].define(version: 20230303192347) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 

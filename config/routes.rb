160e25c590c73987d3c0dce91a3eaeafd0297b10
187jjay187 <187jjay187@gmail.com>
2023-03-10 23:55:52 +0200

    Add form functionality for new comment from a user

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,10 @@ Rails.application.routes.draw do
   # root "articles#index"
 
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show, :new, :create], param: :id
-  end 
+    resources :posts, only: [:index, :show, :new, :create], param: :id do
+      resources :comments, only: [:new, :create]
+    end
+  end
 
   root 'users#index'
 end

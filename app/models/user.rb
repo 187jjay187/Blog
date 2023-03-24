dd5151aa36c6b8024633b28e054bc6906f70a37f
187jjay187 <187jjay187@gmail.com>
2023-03-24 18:40:05 +0200

    Add destroy method and fix for controllers & views

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
-         has_many :posts, class_name: 'Post', foreign_key: 'author_id', dependent: :destroy
-         has_many :comments, class_name: 'Comment', foreign_key: 'author_id', dependent: :destroy
-         has_many :likes, class_name: 'Like', foreign_key: 'author_id', dependent: :destroy
+  has_many :posts, class_name: 'Post', foreign_key: 'author_id', dependent: :destroy
+  has_many :comments, class_name: 'Comment', foreign_key: 'author_id', dependent: :destroy
+  has_many :likes, class_name: 'Like', foreign_key: 'author_id', dependent: :destroy
 
   validates :name, presence: true
   validates :email, presence: true

9e753451893a4df17248113429a2b084bac80b19
187jjay187 <187jjay187@gmail.com>
2023-03-24 12:20:07 +0200

    Add migration file to add devise to users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+userOne = User.create(name: "Jerome", photo: "https://unsplash.com/photos/l-T-LpQnNRg", bio: "full Stack Dev", email: "187jjay187@gmail.com", password: "18989")
+userTwo = User.create(name: "Jerome", photo: "https://unsplash.com/photos/l-T-LpQnNRg", bio: "full Stack Dev", email: "187jjay187@gmail.com", password: "18989")
+userThree = User.create(name: "Jerome", photo: "https://unsplash.com/photos/l-T-LpQnNRg", bio: "full Stack Dev", email: "187jjay187@gmail.com", password: "18989")
+userFour = User.create(name: "Jerome", photo: "https://unsplash.com/photos/l-T-LpQnNRg", bio: "full Stack Dev", email: "187jjay187@gmail.com", password: "18989")
+userFive = User.create(name: "Jerome", photo: "https://unsplash.com/photos/l-T-LpQnNRg", bio: "full Stack Dev", email: "187jjay187@gmail.com", password: "18989")
